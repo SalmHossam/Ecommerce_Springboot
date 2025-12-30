47116ac60e94d58dab315d7582cc072caf9a55ff
SalmaHossam <11410120206031@stud.edu.eg>
2025-12-30 16:25:14 +0200

    email verfication service

--- src/main/java/com/salma/Ecommerce/Service/UserService.java
+++ src/main/java/com/salma/Ecommerce/Service/UserService.java
@@ -3,11 +3,13 @@ package com.salma.Ecommerce.Service;
 import com.salma.Ecommerce.DTO.UserLoginDTO;
 import com.salma.Ecommerce.DTO.UserRegisterDto;
 import com.salma.Ecommerce.Entity.User;
+import com.salma.Ecommerce.Entity.VerificationToken;
 import com.salma.Ecommerce.Exceptions.UserAlreadyExistsException;
 import com.salma.Ecommerce.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.Optional;
 
 @Service
@@ -34,7 +36,18 @@ public class UserService {
         user.setPhoneNumber(userRegisterDto.getPhoneNumber());
         user.setEmail(userRegisterDto.getEmail());
         user.setPassword(encryptService.encryptPassword(userRegisterDto.getPassword()));
+        VerificationToken verificationToken=createVerificationToken(user);
         return userRepository.save(user);
+
+    }
+
+    private VerificationToken createVerificationToken(User user){
+        VerificationToken verificationToken=new VerificationToken();
+        verificationToken.setUser(user);
+        verificationToken.setToken(jwtService.generateEmailJWT(user));
+        verificationToken.setCreatedTimeStamp(new Timestamp(System.currentTimeMillis()));
+        user.getVerificationTokens().add(verificationToken);
+        return verificationToken;
 
     }
     public String loginUser(UserLoginDTO userLoginDTO){
